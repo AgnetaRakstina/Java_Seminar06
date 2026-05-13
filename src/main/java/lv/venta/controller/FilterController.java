96323cc6cffe513038cd0b15c9095938e7e7b73f
AgnetaRakstina <rakstinaagneta@gmail.com>
2026-05-13 10:01:04 +0300

    idk bro

--- src/main/java/lv/venta/controller/FilterController.java
+++ src/main/java/lv/venta/controller/FilterController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lv.venta.model.Course;
+import lv.venta.model.Grade;
 import lv.venta.model.Professor;
 import lv.venta.model.enums.Degree;
 import lv.venta.service.IFilterService;
@@ -34,4 +36,47 @@ public class FilterController {
 	}
 	
 	
-}
+	@GetMapping("/course/professor/{id}")
+	public String getCourseByProfessorID(@PathVariable(name = "id") long id, Model model) {
+		try {
+			ArrayList<Course> coursesFromDB = filterService.filterCourseByProfessorId(id);
+			model.addAttribute("packge", coursesFromDB);
+			return "show-multiple-courses-page";
+			
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	@GetMapping("/grades/student/{name}/{surname}")
+	public String getGradesByStudentNameAndSurname(@PathVariable(name = "name") String name, @PathVariable(name = "surname") String surname, Model model) {
+		try {
+			ArrayList<Grade> gradesFromDB = filterService.filterGradeByStudentNameAndSurname(name, surname);
+			model.addAttribute("package", gradesFromDB);
+			return "show-multiple-grades-page";
+			
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+	//calculateAvgGradeByCourseTitle
+	@GetMapping("/course/title/average")
+	public String getAvrCourseGradeByCourseTitle(@PathVariable(name = "title") String title, Model model) {
+		try {
+			float avgGrade = filterService.calculateAvgGradeByCourseTitle(title);
+			model.addAttribute("package", avgGrade);
+			
+			return "show-average-grade-by-title-page";
+			
+			
+		} catch (Exception e) {
+			model.addAttribute("package", e.getMessage());
+			return "error-page";
+		}
+	}
+	
+}	
+
